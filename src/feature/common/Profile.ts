a8a0984e86911e95cc90d92a96339924de658ed2
gw-doc <2367348689@qq.com>
2022-11-06 18:09:42 +0800

    feat:测试你

--- src/feature/common/Profile.ts
+++ src/feature/common/Profile.ts
@@ -6,7 +6,7 @@ import type Controls from 'three-freeform-controls/dist/types/controls'
 
 export class Profile {
     view: IView
-    
+
     planeMesh: Mesh //用来看的平面
     controls: Controls
     controlsManager: ControlsManager
@@ -58,12 +58,53 @@ export class Profile {
         }
     }
 
+    // 绑定点击事件
+    bindClickEvent() {
+        // 所有拾取的物体
+        const selectObjs = this.view.scene.children.filter((item) => item.type === 'test_model')
+        console.log(selectObjs)
+        this.view.dom.addEventListener('click', (event: MouseEvent) => {
+            const mouse = { x: 0, y: 0 }
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+            const raycaster = new Raycaster()
+            raycaster.setFromCamera(mouse, this.view.camera)
+            const intersects = raycaster.intersectObjects(selectObjs)
+            if (intersects.length) {
+                const selObj: Intersection = intersects[0]
+                // 根据选中的信息 初始化法向和距离
+                this.selectNormal = selObj.face!.normal.clone().normalize()
+                // 取三个点构成平面 求距离
+                // this.selectDistance = selObj.distance
+                const selMesh = selObj.object as Mesh
+                const face = selObj.face!
+                const posAttr = selMesh.geometry.getAttribute('position') as BufferAttribute
+                const point1 = this.getPointFromArr(face.a, posAttr)
+                const point2 = this.getPointFromArr(face.b, posAttr)
+                const point3 = this.getPointFromArr(face.c, posAttr)
+                const tmpPlane = new Plane()
+                tmpPlane.setFromCoplanarPoints(point1, point2, point3)
+                this.selectDistance = tmpPlane.constant
+
+                //创建切面和切面辅助面
+                this.create_plane()
+
+                // 创建控制器和mesh
+                this.create_planeMesh()
+            }
+        })
+    }
+
     // 创建mesh和控制器
     create_planeMesh() {
         // 可见的mesh
         const planeGeo = new PlaneGeometry(this.planeWidth, this.planeHeight)
         const material = this.getMeshmaterial('#ffff33')
         this.planeMesh = new Mesh(planeGeo, material)
+        
+        // 创建时的变换矩阵
+        this.planeMesh.updateMatrixWorld()
+
         this.view.scene.add(this.planeMesh)
 
         // 控制器
@@ -82,28 +123,9 @@ export class Profile {
     // 创建切面和切面辅助面
     create_plane() {
         //FIXME  剖切面 重新设置距离
-        this.plane = new Plane(this.selectNormal, 0)
+        this.plane = new Plane(this.selectNormal, this.selectDistance)
         this.planeHelper = new PlaneHelper(this.plane, 60, 0xff0000)
         this.view.scene.add(this.planeHelper)
-    }
-
-    // 绑定点击事件
-    bindClickEvent() {
-        // 所有拾取的物体
-        const selectObjs = this.view.scene.children.filter((item) => item.type === 'test_model')
-        console.log(selectObjs)
-        this.view.dom.addEventListener('click', (event: MouseEvent) => {
-            const mouse = { x: 0, y: 0 }
-            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-            const raycaster = new Raycaster()
-            raycaster.setFromCamera(mouse, this.view.camera)
-            const intersects = raycaster.intersectObjects(selectObjs)
-            if (intersects.length) {
-                const selObj: Intersection = intersects[0]
-                // 根据选中的信息 创建
-            }
-        })
     }
 
     // 绑定控制轴事件
@@ -111,6 +133,14 @@ export class Profile {
         this.controlsManager.listen(EVENTS.DRAG_START, (planeMesh, handleName) => {
             console.log(handleName)
         })
+    }
+
+    // 从数组中获取点
+    getPointFromArr(index: number, attr: BufferAttribute): Vector3 {
+        const point0 = attr.getX(index)
+        const point1 = attr.getY(index)
+        const point2 = attr.getZ(index)
+        return new Vector3(point0, point1, point2)
     }
 
     getMeshmaterial(color: string): MeshStandardMaterial {
